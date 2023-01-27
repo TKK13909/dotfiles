176cef06db3aa3bdbdec6ff03f6b0cbe6c80b96e
TKK13909 <tkk13909@proton.me>
2023-01-27 08:13:34 -0800

    updated

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo Which user would you like to install this to? 
 read user
 
+echo coppying files to $user
+
 echo Copying rc files
-cp .bashrc .vimrc /home/$user/
+#cp .bashrc .vimrc /home/$user/
