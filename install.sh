360875086564c729c04cf39a9ce28142a4065599
TKK13909 <tkk13909@proton.me>
2023-01-27 08:25:25 -0800

    updated

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 echo Which user would you like to install this to? 
 read user
 
-echo coppying files to $user
+echo Copying files to /home/$user/
 
 echo Copying rc files
-#cp .bashrc .vimrc /home/$user/
+cp .bashrc .vimrc -v /home/$user/
+
+echo Copying .config folder
+cp .config -rv /home/$user/ 
+
+echo Copying .wallpapers folder
+cp .wallpapers -rv /home/$user/
+
+echo Copying .welcome folder
+cp .welcome -rv /home/$user/
